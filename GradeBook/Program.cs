bc0557ef20803c3569ae559c5ae81586174cf63c
amagalla <angelmagallanes18@gmail.com>
2023-08-03 15:58:15 -0500

    Successful implementation of random function.

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,2 +1,22 @@
 ﻿using System;
+using System.Security.Cryptography;
 
+Console.WriteLine("Welcome to GradeBook BAsiC.\n");
+Console.WriteLine("To begin, enter the student's name:\n");
+var name = Console.ReadLine();
+
+//assign random grade to student
+double randomGrade = RandomNumberGenerator.GenerateRandomNumber(0.0, 100.0);
+Console.WriteLine(name + "'s current grade is: " + randomGrade);
+
+
+public class RandomNumberGenerator
+{
+    private static Random randomNum = new Random();
+
+    public static double GenerateRandomNumber(double minValue, double maxValue)
+    {
+        double randomValue = minValue + randomNum.NextDouble() * maxValue;
+        return Math.Round(randomValue, 2);
+    }   
+}
