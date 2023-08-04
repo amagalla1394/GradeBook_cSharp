99fa12ef544754b946eb3c2364323a4751d93b40
amagalla <angelmagallanes18@gmail.com>
2023-08-04 15:05:34 -0500

    adding if-statement for grade to letter conversion

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Security.Cryptography;
 
+
 Console.WriteLine("Welcome to GradeBook BAsiC.\n");
 Console.WriteLine("To begin, enter the student's name:\n");
 var name = Console.ReadLine();
 
 //assign random grade to student
 double randomGrade = RandomNumberGenerator.GenerateRandomNumber(0.0, 100.0);
-Console.WriteLine(name + "'s current grade is: " + randomGrade);
+Console.WriteLine(name + "'s current grade is at " /*+ Letter + " with a "*/ + randomGrade + "%");
 
+
+/*void letter(string letterGrade)
+{
+    if (randomGrade >= 90 && randomGrade <= 100)
+    {
+        letterGrade = "A";
+    }
+    else if (randomGrade >= 80 && randomGrade < 90)
+    {
+
+    }
+    else if (randomGrade >= 70 && randomGrade < 80)
+    {
+
+    }
+    else if (randomGrade >= 60 && randomGrade < 70)
+    {
+
+    }
+    else
+    {
+
+    }
+}*/
 
 public class RandomNumberGenerator
 {
@@ -20,3 +45,4 @@ public class RandomNumberGenerator
         return Math.Round(randomValue, 2);
     }   
 }
+
